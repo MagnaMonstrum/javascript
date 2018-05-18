feed1dc356dbc511afd3f6cca9ed0d414471e57a
Mo <midbid@roc-dev.com>
2018-05-18 11:46:58 +0200

    Javascript H1 t/m H2

---
+++ stijl.js
@@ -0,0 +1,11 @@
+// Opdracht 2.2
+
+bl1.style.backgroundColor = "#00ccff";
+bl1.style.padding = "20px";
+bl2.style.paddingLeft = "20px";
+bl2.style.backgroundColor = "green";
+bl2.style.marginTop = "10px";
+bl3.style.paddingLeft = "43px";
+bl3.style.marginTop = "40px";
+bl3.style.backgroundColor = "yellow";
+bl4.style.backgroundColor = "red";
