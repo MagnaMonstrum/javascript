47e86e3b38f6b1c74327caa3d51c65300d4c592e
Mo <midbid@roc-dev.com>
2018-05-18 09:37:53 +0200

    Javascript H1

--- mijnjavascript.js
+++ mijnjavascript.js
@@ -4,8 +4,14 @@ var mijnauto = {
     aantalWielen: 4,
     kleur: "blauw"
 }
+
+//Opdracht 1.1
 console.log("Hello World");
+
+//Opdracht 1.2
 console.log(mijnauto.kleur);
+
+//Opdracht1.3
 for(var i = 0; i < 10; i++){
     console.log("i heeft nu de waarde " + i);
 }
