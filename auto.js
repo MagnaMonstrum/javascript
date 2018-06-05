fcc717d28272d49672fa289b9487c7a30d27bdd0
Mo <midbid@roc-dev.com>
2018-06-05 09:01:38 +0200

    Javascript H1 t/m H3

---
+++ auto.js
@@ -0,0 +1,23 @@
+var mijnauto = {
+    kleur: "rood",
+    merk: "Ford",
+    snelheid: 0,
+
+    gasgeven: function(){
+        this.snelheid += 5
+        console.log(this.snelheid);
+
+    },
+
+    toeteren: function () {
+        console.log("toet!")
+    }
+
+}
+console.log(mijnauto.kleur);
+mijnauto.gasgeven();
+mijnauto.gasgeven();
+mijnauto.gasgeven();
+mijnauto.gasgeven();
+mijnauto.gasgeven();
+mijnauto.toeteren();
